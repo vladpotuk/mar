407ec77ac0f0d8e520b14085d2b87ed32c913894
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-04-26 22:12:04 +0300

    Create SQLQuery1.sql

---
+++ SQLQuery1.sql
@@ -0,0 +1,39 @@
+USE Marvel;
+GO
+
+-- Deletion Script
+
+USE Marvel;
+GO
+
+-- Delete data from Relationships table
+DELETE FROM Relationships;
+
+-- Delete data from Creators table
+DELETE FROM Creators;
+
+-- Delete data from Series table
+DELETE FROM Series;
+
+-- Delete data from Movies table
+DELETE FROM Movies;
+
+-- Delete data from Events table
+DELETE FROM Events;
+
+-- Delete data from Powers table
+DELETE FROM Powers;
+
+-- Delete data from Teams table
+DELETE FROM Teams;
+
+-- Delete data from Comics table
+DELETE FROM Comics;
+
+-- Delete data from Characters table
+DELETE FROM Characters;
+
+-- Delete data from Origins table
+DELETE FROM Origins;
+
+
